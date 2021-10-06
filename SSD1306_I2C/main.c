03fb50b37222b4484764f9a1d444de983b096435
AlexKar0706 <53472100+AlexKar0706@users.noreply.github.com>
2021-10-06 20:02:03 +0300

    Remove unused function + fix cursor

--- SSD1306_I2C/main.c
+++ SSD1306_I2C/main.c
@@ -1,15 +1,9 @@
 #include "tm4c123gh6pm.h"
 #include "SSD1306.h"
 
-void Delay1ms(unsigned long ms) {
-	unsigned long time = ms * 13333;
-	while (time)
-		time--;
-}
-
 int main() {
 	SSD1306_Init();
-	SSD1306_SetCursor(3, 4);
+	SSD1306_SetCursor(4, 3);
 	SSD1306_Write_Str("Hello, world!");
 	while (1) {
 	}
